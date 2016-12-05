067fc0a885e8d73d0c1563b27bdf2c8b68eccd88
chenming <583431043@qq.com>
2016-12-05 14:00:45 +0800

    更新AAR文件

--- app/src/main/java/com/qicode/signsdkdemo/MyApplication.java
+++ app/src/main/java/com/qicode/signsdkdemo/MyApplication.java
@@ -13,6 +13,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         //测试版、发布版的商户key和秘钥不同，注意打正式包的时候换成发布版的商户key和秘钥
-        QiCodeApi.init(this, "key_xxxxxxxxxxx", "secret_xxxxxxxxxxxxxx");
+        QiCodeApi.init(this, "key_*******", "secret_*******");
     }
 }
