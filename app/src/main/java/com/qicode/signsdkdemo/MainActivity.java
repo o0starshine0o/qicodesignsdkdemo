619fadbc1efae1e243cf21564da94f3d2e104e01
chenming <583431043@qq.com>
2016-12-07 11:59:40 +0800

    1.标题栏UI微调 2.添加Fragement调用

--- app/src/main/java/com/qicode/signsdkdemo/MainActivity.java
+++ app/src/main/java/com/qicode/signsdkdemo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.qicode.signsdkdemo;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.qicode.mylibrary.QiCodeApi;
+import com.qicode.mylibrary.fragment.ExpertSignListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
                 QiCodeApi.invokeSignPage(MainActivity.this);
             }
         });
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.ll_content, new ExpertSignListFragment());
+        fragmentTransaction.commit();
     }
 }
