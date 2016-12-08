448bf4a1c21d38d665d11b18e71c4355f30e3588
chenming <583431043@qq.com>
2016-12-08 12:55:28 +0800

    aar发布到jcenter，一行代码集成sdk

--- app/src/main/java/com/qicode/signsdkdemo/MyApplication.java
+++ app/src/main/java/com/qicode/signsdkdemo/MyApplication.java
@@ -13,6 +13,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         //测试版、发布版的商户key和秘钥不同，注意打正式包的时候换成发布版的商户key和秘钥
-        QiCodeApi.init(this, "key_*******", "secret_*******");
+        QiCodeApi.init(this, "key_c219bbde461c49c52f847e19149d784d", "secret_a6a05183fc1e92f0a5fdb13f0a14cef5");//test
     }
 }
