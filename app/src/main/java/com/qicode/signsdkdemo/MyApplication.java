2c28d7e5947bcd52515d954f3d8424ba764f62eb
chenming <583431043@qq.com>
2016-12-08 13:53:14 +0800

    删除无用代码

--- app/src/main/java/com/qicode/signsdkdemo/MyApplication.java
+++ app/src/main/java/com/qicode/signsdkdemo/MyApplication.java
@@ -13,7 +13,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         //测试版、发布版的商户key和秘钥不同，注意打正式包的时候换成发布版的商户key和秘钥
-        QiCodeApi.init(this, "key_c219bbde461c49c52f847e19149d784d", "secret_a6a05183fc1e92f0a5fdb13f0a14cef5");//test
-//        QiCodeApi.init(this, "key_2048b3aba40d686a1805fbc52bf971b2", "secret_99dd5cc6b374d2873a8bbf7333575973");//release
+        QiCodeApi.init(this, "key_*******", "secret_*****");//test
     }
 }
